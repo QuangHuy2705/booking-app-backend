d1dd218a6b602e578e97c6fdf3711a096bf63bab
QuangHuy <ntquanghuy2705@gmail.com>
2019-07-19 10:42:09 +0300

    edit port

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 //ERROR HANDLING
 app.use(errorHandler)
 
-app.listen(80, () => {
+app.listen(PORT, () => {
     console.log(`SERVER is listening on PORT ${PORT}`)
 })
 
